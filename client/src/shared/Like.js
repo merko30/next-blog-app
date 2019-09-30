3563effd186335d8e666b1ce91581c3977142b80
Merim Hasanbegovic <merim.hasanbegovic@outlook.com>
2019-09-30 13:06:06 +0200

    fix like text error

--- client/src/shared/Like.js
+++ client/src/shared/Like.js
@@ -3,33 +3,50 @@ import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 
-import crypto from "crypto";
-
 const Like = ({ onClick, post, user }) => {
-  const liked = post.likes.includes(user._id);
+  const liked = user && post.likes.includes(user._id);
   const color = liked ? "gray" : "green";
-  const text = liked
-    ? post.likes.length === 1
-      ? "You like this post"
-      : `You and ${post.likes.length - 1} like this post`
-    : post.likes.length === 0
-    ? `Still no likes`
-    : `${post.likes.length} like this post`;
-
+  const personOrPeople = length =>
+    length > 1 ? `${length} people` : `${length} person`;
+  const text = () => {
+    if (user && liked) {
+      if (post.likes.length === 1) {
+        return "You like this post";
+      } else {
+        return `You and ${personOrPeople(
+          post.likes.length - 1
+        )} like this post`;
+      }
+    } else {
+      if (post.likes.length === 0) {
+        return "Still no likes";
+      } else {
+        return `${personOrPeople(post.likes.length)} ${
+          post.likes.length > 1 ? "like" : "likes"
+        } this post`;
+      }
+    }
+  };
   return (
     <div className={`p-2 flex items-center`}>
-      <div
+      <span
         className={`cursor-pointer border border-${color}-600 flex items-center justify-center p-3 mr-2 rounded-full`}
       >
         <FontAwesomeIcon
           title={liked ? "Dislike" : "Like"}
           icon={faThumbsUp}
           size={"lg"}
-          onClick={() => onClick(post._id)}
+          onClick={e => {
+            if (user) {
+              onClick(post._id);
+            } else {
+              e.stopPropagation();
+            }
+          }}
           color={color}
         />
-      </div>
-      <p className="text-sm ml-2">{text}</p>
+      </span>
+      <p className="text-sm ml-2">{text()}</p>
     </div>
   );
 };
