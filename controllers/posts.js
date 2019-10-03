210188ca12234e797e27b072d34b6b0f6348f774
Merim Hasanbegovic <merim.hasanbegovic@outlook.com>
2019-10-03 11:07:43 +0200

    upload

--- controllers/posts.js
+++ controllers/posts.js
@@ -68,7 +68,7 @@ const update = async (req, res, next) => {
     const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
       new: true
     });
-    if (req.file) {
+    if (req.file && req.file !== null) {
       post.image = req.file.filename;
     }
     await post.save();
