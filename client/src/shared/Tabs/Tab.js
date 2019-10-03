a3c0ec519bcb37411a8976cb1f8ae243437532c5
Merim Hasanbegovic <merim.hasanbegovic@outlook.com>
2019-10-03 15:43:26 +0200

    tabs

--- client/src/shared/Tabs/Tab.js
+++ client/src/shared/Tabs/Tab.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Tab = ({ title, children, classes }) => (
+const Tab = ({ title, children, classes }) => {
   Tab.displayName = "Tab";
 
-  <div className={classes}>{children}</div>
-);
+  return <div className={classes}>{children}</div>;
+};
 
 export default Tab;
