aef641799a80959ce9d70adcff5c0ecfc6053f0e
Merim Hasanbegovic <merim.hasanbegovic@outlook.com>
2019-10-03 10:48:24 +0200

    port

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ if (process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
+  console.log(`Application running on port ${PORT}`);
 });
