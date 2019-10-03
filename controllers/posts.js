caca1d117711ed99fd0ae36ba55b5d92c7376022
Merim Hasanbegovic <merim.hasanbegovic@outlook.com>
2019-10-03 13:19:34 +0200

    file error fix

--- controllers/posts.js
+++ controllers/posts.js
@@ -68,7 +68,7 @@ const update = async (req, res, next) => {
     const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
       new: true
     });
-    if (req.file !== null && req.file !== "null") {
+    if (req.file != null) {
       post.image = req.file.filename;
     }
     await post.save();
