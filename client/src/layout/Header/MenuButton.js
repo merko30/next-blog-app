cb708ec39fc9ce403f9e193fb19e3a1557c980d5
Merim Hasanbegovic <merim.hasanbegovic@outlook.com>
2019-10-04 12:13:06 +0200

    navbar

--- client/src/layout/Header/MenuButton.js
+++ client/src/layout/Header/MenuButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const MenuButton = ({ onClick }) => (
   <button
     onClick={onClick}
-    className="absolute right-0 top-0 m-6 focus:outline-none"
+    className="absolute right-0 top-0 m-6 focus:outline-none md:hidden"
   >
     <span className="block bg-black w-8 h-1"></span>
     <span className="block bg-black my-1 w-8 h-1"></span>
