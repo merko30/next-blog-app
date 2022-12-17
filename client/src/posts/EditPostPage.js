1ab4ab57bc033bd500b9272132afcf84a0281bf1
Merim <merim.hasanbegovic@outlook.com>
2022-12-17 21:36:34 +0100

    feat: create edit post page
    Signed-off-by: Merim <merim.hasanbegovic@outlook.com>

---
+++ client/src/posts/EditPostPage.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useMutation, useQuery } from "react-query";
+import { useNavigate, useParams } from "react-router-dom";
+
+import transformObjectToFormData from "../utils/transformObjectToFormData";
+
+import { getPost, updatePost } from "./posts.actions";
+
+import Error from "../shared/Error";
+import Loading from "../shared/Loading";
+
+import PostForm from "./PostForm";
+
+const EditPostPage = () => {
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const {
+    data: queryData,
+    isLoading,
+    error,
+  } = useQuery("post", () => getPost(id));
+
+  const { mutate } = useMutation(
+    (values) => {
+      const formData = transformObjectToFormData(values);
+
+      updatePost(formData);
+    },
+    {
+      onSuccess: () => navigate("/"),
+    }
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error error="Failed to load the post. Try reloading" />;
+  }
+
+  if (queryData) {
+    const {
+      data: { post },
+    } = queryData;
+    return <PostForm post={post} onSubmit={mutate} />;
+  }
+};
+
+export default EditPostPage;
--- client/src/posts/posts.actions.js
+++ client/src/posts/posts.actions.js
@@ -1,14 +1,5 @@
 import Axios from "axios";
 
-import createAction from "../utils/createAction";
-import populateFormData from "../utils/transformObjectToFormData";
-
-export const getPostsAction = createAction("GET_POSTS");
-export const getPostAction = createAction("GET_POST");
-export const addPostAction = createAction("ADD_POST");
-export const updatePostAction = createAction("EDIT_POST");
-export const likePostAction = createAction("LIKE_POST");
-
 const url = process.env.REACT_APP_API_URL;
 
 export const getPosts = async (page = 1) => {
@@ -21,28 +12,8 @@ export const getPost = async (id) => await Axios.get(`${url}/posts/${id}`);
 export const addPost = async (values) =>
   await Axios.post(`${url}/posts`, values, { withCredentials: true });
 
-export const updatePost = (id, p) => async (dispatch) => {
-  dispatch(updatePostAction.start());
-  try {
-    const { image, ...rest } = p;
-    const d = p.image ? populateFormData(p) : rest;
-    const {
-      data: { post },
-    } = await Axios.put(`${url}/posts/${id}`, d);
-    dispatch(updatePostAction.success(post));
-  } catch (error) {
-    dispatch(updatePostAction.failure(error.response.data.message));
-  }
-};
+export const updatePost = async (id, values) =>
+  await Axios.put(`${url}/posts/${id}`, values);
 
-export const likePost = (id) => async (dispatch) => {
-  dispatch(likePostAction.start());
-  try {
-    const {
-      data: { post },
-    } = await Axios.put(`${url}/posts/${id}/like`);
-    dispatch(likePostAction.success(post));
-  } catch (error) {
-    dispatch(likePostAction.failure(error.response.data.message));
-  }
-};
+export const likePost = async (id) =>
+  await Axios.put(`${url}/posts/${id}/like`);
