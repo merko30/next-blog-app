26dba47d4375fa747d5fe34ed720e3937abcedf6
Merim <merim.hasanbegovic@outlook.com>
2026-05-23 18:43:34 +0200

    fix: middleware
    Signed-off-by: Merim <merim.hasanbegovic@outlook.com>

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,6 @@
-export { default } from "next-auth/middleware";
+import middleware from "next-auth/middleware";
+
+export default middleware;
 
 export const config = {
   matcher: ["/profile", "/profile/:path*", "/create/:path*", "/posts/:id/edit"],
